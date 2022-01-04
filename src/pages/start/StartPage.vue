6823527ee3c9d6066a60ed26d6beddb62d0692bf
jeremias ramirez <jereramirez499@gmail.com>
2022-01-04 12:28:15 -0400

    creado loader init app

--- src/pages/start/StartPage.vue
+++ src/pages/start/StartPage.vue
@@ -1,11 +1,56 @@
 <template>
     <div>
-        start page mounted
+        <img class="logo" src="../../../public/logo.png" alt="logo">
     </div>
+
+     <section class="container__spinning__loading">
+        <article class="spinning__loading"></article>
+    </section> 
 </template>
 
 <script>
 export default {
-    name: 'StartPage'
+    name: 'StartPage',
+    mounted(){
+        let i=0;
+        let loading=setInterval(()=>{
+            i++;
+            if(i==2){
+                 clearInterval(loading);
+                 this.$router.push("/home")
+            }
+
+        },2000);
+    }
 }
 </script>
+
+<style>
+    .logo{
+        border-radius:50%;
+    }
+
+     .container__spinning__loading{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-top:30px;
+        }    
+        .spinning__loading{
+            width: 30px;
+            height: 30px;
+            border-radius: 50%;
+            border-left:3px solid rgb(94, 128, 238); 
+            animation: scaleOnScale .4s infinite linear;
+        }
+
+      
+        @keyframes scaleOnScale {
+            from{
+                transform: rotate(-360deg);
+            }
+            to{
+                transform: rotate(0);
+            }
+        }
+</style> 
