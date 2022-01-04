b4d0f6a6ca849c0130bffa6669d03281e3d5e01b
jeremias ramirez <jereramirez499@gmail.com>
2022-01-04 11:46:24 -0400

    funcion delete para cada stacks

--- src/pages/home/home.page.vue
+++ src/pages/home/home.page.vue
@@ -47,8 +47,9 @@
                     
                      
 
-                        <button class="option" v-on:click="moveTo(task)">
+                        <button class="option" >
                            <img src="../../assets/images/move-to.svg" alt="option">
+                         
                        </button>
                         
                         
@@ -73,11 +74,11 @@
      
 
      <!-- doing stacks -->
-            <!-- pending stacks-->
+           
         <article class="pending" >
             
             <div class="title__module">
-                <h1>Pending</h1> 
+                <h1>Doing</h1> 
             </div>
             
             <div class="no-item" v-if="!doing.length">
@@ -114,10 +115,73 @@
 
                         <button class="option" v-on:click="moveTo(task)">
                            <img src="../../assets/images/move-to.svg" alt="option">
+                         
                        </button>
                         
                         
                         <button class="option" v-on:click="deleteDoing(index)">
+                           <img src="../../assets/images/delete.svg" alt="option">
+                       </button>
+
+
+                       <button class="option" v-on:click="complete(task)">
+                           <img src="../../assets/images/do.svg" alt="option">
+                       </button>
+
+                        
+                          <button class="option">
+                           <img src="../../assets/images/resize.svg" alt="option">
+                       </button>
+                   </div>
+            </div>
+
+        </article>
+
+
+
+
+
+           <!-- do stacks -->
+           
+        <article class="pending" >
+            
+            <div class="title__module">
+                <h1>Do</h1> 
+            </div>
+            
+            <div class="no-item" v-if="!dod.length">
+                <img src="../../assets/images/do.svg" alt="">
+                
+            </div>
+
+            <div class="task" v-for="( task, index ) in dod" :key="index">
+                <div class="title">
+                    <h1 class="title__task">{{ task.title }}</h1> 
+                    <span class="date__task">{{ task.date }}</span>
+                </div>
+
+                <!--content task  -->
+                <div class="content">
+                       <p class="content__task">
+                         {{ task.content}}
+                    </p>
+                </div>
+
+
+                   <!-- options task  -->
+                   <div class="options__task">
+                      <button class="option">
+                           <img src="../../assets/images/create.svg" alt="option">
+                       </button>
+
+
+                       <button class="option">
+                           <img src="../../assets/images/share.svg" alt="option">
+                       </button>
+                    
+                    
+                        
+                        <button class="option" v-on:click="deleteDod(index)">
                            <img src="../../assets/images/delete.svg" alt="option">
                        </button>
 
@@ -157,6 +221,9 @@ export default {
             deleteDoing: (index)=>{
                 this.doing.splice(index,1)
             },
+              deleteDod: (index)=>{
+                this.dod.splice(index,1)
+            },
 
              moveTo: (task)=>{
                  console.log(task)
@@ -166,6 +233,14 @@ export default {
 
             complete:(task)=>{
                 console.log(task)
+            },
+            moveToDoing: (task,index)=>{
+                console.log(task)
+                console.log(index)
+            },
+             moveToDo: (task,index)=>{
+                console.log(task)
+                console.log(index)
             },
 
 
@@ -185,8 +260,12 @@ export default {
                 {title: "Databases II", content: "Leer libro la genetica y biologia basica de maxwell.", date: "2 Dec 2021"}
             
             ],
-            do: [
-
+            dod: [
+                {title: "Phisic", content: "Hacer los ejercicios de calculos y geometria analitica", date: "29 march 2021"},
+                {title: "German", content: "Leer libro la genetica y biologia basica de maxwell.", date: "2 Dec 2021"},
+                {title: "Francais", content: "Hacer los ejercicios de calculos y geometria analitica", date: "29 march 2021"},
+                {title: "Admin I", content: "Leer libro la genetica y biologia basica de maxwell.", date: "2 Dec 2021"}
+            
             ]
         }
     },
